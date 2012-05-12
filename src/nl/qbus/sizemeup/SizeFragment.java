a9047e82f27d91ec4891231341ee994f323b1866
hvisser <hvisser@qbus-ict.nl>
2012-05-12 23:44:44 +0200

    Initial version

---
+++ src/nl/qbus/sizemeup/SizeFragment.java
@@ -0,0 +1,53 @@
+package nl.qbus.sizemeup;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class SizeFragment extends Fragment {
+    private TextView mLabelArrowWidth;
+    private TextView mLabelArrowHeight;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.size_view, container, false);
+
+        TextView screenDimensionPixels = (TextView) view.findViewById(R.id.screenDimensionsPixels);
+        TextView screenDimensionDips = (TextView) view.findViewById(R.id.screenDimensionsDips);
+        mLabelArrowWidth = (TextView) view.findViewById(R.id.horizontal_label);
+        mLabelArrowHeight = (TextView) view.findViewById(R.id.vertical_label);
+
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        screenDimensionPixels.setText(getString(R.string.screen_dimensions_pixels, metrics.widthPixels, metrics.heightPixels));
+        screenDimensionDips.setText(getString(R.string.screen_dimensions_dips, metrics.widthPixels/metrics.density, metrics.heightPixels/metrics.density));
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.post(new Runnable() {
+            @Override
+            public void run() {
+                showViewDimensions();
+            }
+        });
+    }
+
+    protected void showViewDimensions() {
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        int width = getView().getWidth();
+        int height = getView().getHeight();
+        float widthDp = width/metrics.density;
+        float heightDp = height/metrics.density;
+
+        mLabelArrowWidth.setText(getString(R.string.view_width, widthDp, width));
+        mLabelArrowHeight.setText(getString(R.string.view_height, heightDp, height));
+    }
+
+
+}
---
+++ src/nl/qbus/sizemeup/SizemeupActivity.java
@@ -0,0 +1,14 @@
+package nl.qbus.sizemeup;
+
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class SizemeupActivity extends FragmentActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
